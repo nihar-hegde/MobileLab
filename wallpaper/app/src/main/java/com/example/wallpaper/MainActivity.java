b29c2f62786ffec73b76f90e3e155f60f794510a
Nihar Hegde <niharhegde163@gmail.com>
2022-06-03 11:47:22 +0530

    adding java code

--- wallpaper/app/src/main/java/com/example/wallpaper/MainActivity.java
+++ wallpaper/app/src/main/java/com/example/wallpaper/MainActivity.java
@@ -2,9 +2,19 @@ package com.example.wallpaper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.WallpaperManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
+    Button changeWallpaper;
+    Timer mytimer;
+    Drawable drawable;
+    WallpaperManager wpm;
+    int prev=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
